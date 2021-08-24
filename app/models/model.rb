55eb44af524e9b30c3e87466929469b2f05f31b1
Jury Razumau <jury.razumau@zendesk.com>
2021-08-24 22:43:32 +0200

    fix release queries

--- app/models/model.rb
+++ app/models/model.rb
@@ -7,15 +7,16 @@ class Model < ApplicationRecord
       where release_details_id = $1
       order by r.rating desc
     SQL
-    ActiveRecord::Base.connection.exec_query(sql, binds: [[nil, release_id]])
+    ActiveRecord::Base.connection.exec_query(sql, "", [[nil, release_id]])
   end
 
   def all_teams_for_latest_release
     sql = <<~SQL
       select team_id, t.title as name, r.rating, r.rating_change
       from #{name}.releases r
-      left join public.rating_team t on r.team_id = t.r_id 
-      where r.release_details_id = (select max(release_details_id) from #{name}.releases)
+      left join public.rating_team t on r.team_id = t.r_id
+      join #{name}.release_details rd on r.release_details_id = rd.id
+      join (select max(date) as max_date from #{name}.release_details) rd_max on rd.date = rd_max.max_date
       order by r.rating desc
     SQL
     ActiveRecord::Base.connection.exec_query(sql)

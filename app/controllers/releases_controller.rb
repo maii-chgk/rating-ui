eddb352c960df856e3dde62181450bc8928239fb
Jury Razumau <jury.razumau@zendesk.com>
2021-09-02 21:11:33 +0200

    format date for current release

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -2,25 +2,24 @@ class ReleasesController < ApplicationController
   DEFAULT_MODEL = "b".freeze
 
   def show
-    fetch_model_data!
     @release_id = params[:release_id].to_i
-    @release_date = @all_releases.find { |r| r['id'] == @release_id }['date']
-    @teams = current_model.all_teams_for_release(@release_id)
+    render_release
   end
 
   def latest
-    fetch_model_data!
-    @release_id, @release_date = current_model.latest_release.values_at("id", "date")
+    @release_id = current_model.latest_release["id"]
+    render_release
+  end
+
+  def render_release
+    @model_name = current_model.name
+    @all_releases = current_model.all_releases
+    @release_date = l(@all_releases.find { |r| r['id'] == @release_id }['date'].to_date)
     @teams = current_model.all_teams_for_release(@release_id)
     render :show
   end
 
   def current_model
     Model.find_by(name: params[:model] || DEFAULT_MODEL)
-  end
-
-  def fetch_model_data!
-    @model_name = current_model.name
-    @all_releases = current_model.all_releases
   end
 end

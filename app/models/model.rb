06c08d668d4aa3025889544ee97c5580dd05c68d
Jury Razumau <jury.razumau@zendesk.com>
2021-09-25 22:59:28 +0200

    group new tournaments by release

--- app/models/model.rb
+++ app/models/model.rb
@@ -90,31 +90,44 @@ class Model < ApplicationRecord
 
   def team_tournaments(team_id:)
     sql = <<~SQL
+      with ranked as (
+        select rank() over (partition by release_id order by rating desc) as place,
+               team_id, rating, rating_change, release_id
+        from b.team_rating
+      )
+      
+      select rel.id as release_id, rel.date as release_date, rating.place as release_place,
+             rating.rating as release_rating, rating.rating_change as release_rating_change,
+             t.id as id, t.title as name, t.end_datetime as date,
+              r.position as place, tr.rating, tr.rating_change
+      from b.release rel
+      left join ranked rating on rating.team_id = $1 and rating.release_id = rel.id
+      left join public.rating_tournament t on t.end_datetime < rel.date and t.end_datetime >= rel.date - interval '7 days'
+      left join public.rating_result r on r.tournament_id = t.id
+      left join b.tournament_result tr on tr.tournament_id = t.id and r.team_id = tr.team_id
+      where r.team_id = $1
+          and r.position != 0
+          and t.maii_rating = true
+      order by rel.date desc, t.end_datetime desc;
+    SQL
+
+    exec_query_with_cache(query: sql, params: [[nil, team_id]], cache_key: "#{name}/#{team_id}/tournaments").to_a
+  end
+
+  def old_tournaments(team_id:)
+    sql = <<~SQL
       select t.id as id, t.title as name, t.end_datetime as date,
-        r.position as place,
-        case 
-          when t.maii_rating = true then tr.rating
-          when t_old_rating_flag.in_old_rating = true then ror.b
-          else null
-        end as rating,
-        case 
-          when t.maii_rating = true then tr.rating_change
-          when t_old_rating_flag.in_old_rating = true then ror.d
-          else null
-        end as rating_change
+        r.position as place, ror.b as rating, ror.d as rating_change
       from public.rating_tournament t
       left join public.tournaments t_old_rating_flag on t.id = t_old_rating_flag.id
       left join public.rating_result r on r.team_id = $1 and r.tournament_id = t.id
       left join public.rating_oldteamrating ror on ror.result_id = r.id
-      left join #{name}.tournament_result tr 
-        on tr.tournament_id = t.id and r.team_id = tr.team_id
       where r.team_id = $1
-        and r.position != 0
-        and (t_old_rating_flag.in_old_rating = true or t.maii_rating = true)
+        and t_old_rating_flag.in_old_rating = true
       order by t.end_datetime desc
     SQL
 
-    exec_query_with_cache(query: sql, params: [[nil, team_id]], cache_key: "#{name}/#{team_id}/tournaments").to_a
+    exec_query_with_cache(query: sql, params: [[nil, team_id]], cache_key: "#{name}/#{team_id}/old_tournaments").to_a
   end
 
   def team_details(team_id:)

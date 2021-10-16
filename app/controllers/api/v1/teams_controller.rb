22afc8ea58b859b0604c886c0b5a0281377b5cd5
Jury Razumau <jury.razumau@zendesk.com>
2021-10-16 16:22:10 +0200

    move page calculations to ApiController

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,24 +1,10 @@
-class Api::V1::TeamsController < ApplicationController
+class Api::V1::TeamsController < ApiController
   include InModel
-
-  TEAMS_PER_PAGE = 500
 
   def show
     @release_id = params[:release_id]
-    teams = current_model.teams_for_release_api(release_id: @release_id, limit: TEAMS_PER_PAGE, offset: offset)
-    render json: metadata.merge({ items: teams }), status: 200
-  end
-
-  def clean_params
-    params.permit(:model, :release_id, :page)
-  end
-
-  def page
-    (clean_params[:page] || 1).to_i
-  end
-
-  def offset
-    (page - 1) * TEAMS_PER_PAGE
+    teams = current_model.teams_for_release_api(release_id: @release_id, limit: PER_PAGE, offset: offset)
+    render_json(metadata: metadata, items: teams)
   end
 
   def metadata

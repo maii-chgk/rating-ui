f5df3c767e7e07a35adc836d7eb078b5301c09fb
Jury Razumau <jury.razumau@zendesk.com>
2021-11-06 20:58:46 +0100

    filter teams by city

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,37 +1,47 @@
 class ReleasesController < ApplicationController
-  before_action :force_trailing_slash
-
   include InModel
 
   def show
     id = clean_params[:release_id] || current_model&.latest_release_id
     return render_404 if id.nil?
 
-    teams = current_model.teams_for_release(release_id: id, top_place: top_place, bottom_place: bottom_place)
-    all_teams_count = current_model.count_all_teams_in_release(release_id: id)
-    @release = ReleasePresenter.new(id: id, teams: teams, teams_in_release_count: all_teams_count)
+    teams = current_model.teams_for_release(release_id: id, from: from, to: to, city: city)
+    @release = ReleasePresenter.new(id: id, teams: teams)
+
+    all_teams_count = current_model.count_all_teams_in_release(release_id: id, city: city)
+    @paging = Paging.new(items_count: all_teams_count, from: from, to: to)
+
+    @filtered = city.present?
 
     @releases_in_dropdown = list_releases_for_dropdown
     @model_name = current_model.name
   end
 
   def clean_params
-    params.permit(:model, :release_id, :from, :to)
+    params.permit(:model, :release_id, :from, :to, :name, :city)
   end
 
-  def top_place
-    @top_place = (clean_params[:from] || 1).to_i
+  def from
+    (clean_params[:from] || 1).to_i
   end
 
-  def bottom_place
-    @bottom_place = (clean_params[:to] || 100).to_i
+  def to
+    (clean_params[:to] || 100).to_i
+  end
+
+  def city
+    @city = clean_params[:city]
+  end
+
+  def filters
+    clean_params
   end
 
   def list_releases_for_dropdown
     current_model.all_releases.map do |release|
       [
         I18n.l(release["date"].to_date),
-        release_path(release_id: release["id"])
+        release_path(release_id: release["id"], city: city)
       ]
     end
   end

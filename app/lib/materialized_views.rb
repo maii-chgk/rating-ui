4f5f3b5a60c5f039743148424c4b2262fd3024ca
Jury Razumau <jury.razumau@zendesk.com>
2022-04-18 12:30:02 +0200

    create indexes for materialized views

--- app/lib/materialized_views.rb
+++ app/lib/materialized_views.rb
@@ -1,5 +1,5 @@
 class MaterializedViews
-  ViewDefinition = Struct.new('ViewDefinition', :name, :query, keyword_init: true)
+  ViewDefinition = Struct.new('ViewDefinition', :name, :query, :index_columns, keyword_init: true)
 
   def self.recreate_all(model:)
     MaterializedViews.new(model).recreate_all
@@ -26,30 +26,37 @@ class MaterializedViews
       with no data
     SQL
     refresh_query = "refresh materialized view #{@model}.#{definition.name}"
+    index_queries = definition.index_columns.map do |column|
+      index_name = "#{definition.name}_#{column}_idx"
+      "create index if not exists #{index_name} on #{@model}.#{definition.name} (#{column})"
+    end
 
     ActiveRecord::Base.connection.exec_query(create_query)
     ActiveRecord::Base.connection.exec_query(refresh_query)
+    index_queries.each { |query| ActiveRecord::Base.connection.exec_query(query) }
   end
 
   def player_ranking
     ViewDefinition.new(
       name: "player_ranking",
+      index_columns: ["player_id"],
       query: <<~SQL
         select rank() over (partition by release_id order by rating desc) as place, 
             player_id, rating, rating_change, release_id
         from #{@model}.player_rating
-    SQL
+      SQL
   )
   end
 
   def team_ranking
     ViewDefinition.new(
       name: "team_ranking",
+      index_columns: ["team_id"],
       query: <<~SQL
         select rank() over (partition by release_id order by rating desc) as place,
             team_id, rating, rating_change, release_id
         from #{@model}.team_rating
-    SQL
+      SQL
     )
   end
 end

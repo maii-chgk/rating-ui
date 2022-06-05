f373901ae8bbc7f8ca306042d82c7c4a621180c7
Jury Razumau <jury.razumau@zendesk.com>
2022-06-05 22:39:24 +0200

    hide/show all rosters

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import RostersController from "./rosters_controller"
+application.register("rosters", RostersController)

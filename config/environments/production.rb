a2b0988e2f535b7dd97b03d9fb8358930a9a7760
Jury Razumau <jury.razumau@zendesk.com>
2022-06-05 14:22:04 +0200

    get secret_key_base from env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'] }
   config.assets.css_compressor = Tailwindcss::Compressor.new(files_with_class_names: Rails.root.glob("app/views/**/*.*"))
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end

b7bac328a3df7a6337735117a1c25072f25822f5
Jury Razumau <mail@razumau.net>
2023-08-05 00:08:46 +0200

    add components to tailwind watched folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
+    './app/components/**/*.{erb,html}',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],

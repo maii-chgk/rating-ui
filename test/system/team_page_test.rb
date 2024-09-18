2a854c481428571292de07a891bc7ad4f4bb74b1
Jury Razumau <mail@razumau.net>
2024-09-18 16:34:01 +0200

    use test fixtures instead of postgres backup
    We use the rating-db-schema image with a database schema instead of running migrations (since they are split between this app and rating-b).

--- test/system/team_page_test.rb
+++ test/system/team_page_test.rb
@@ -2,46 +2,30 @@ require "test_helper"
 require "minitest/autorun"
 
 class TeamPageTest < ActionDispatch::IntegrationTest
+  include ActiveRecord::TestFixtures
   include Capybara::DSL
 
-  def most_recent_september_1st
-    today = Time.zone.today
-    year = (today.month >= 9) ? today.year : today.year - 1
-    Time.zone.local(year, 9, 1)
-  end
+  fixtures :seasons, :teams, :players, :towns
 
   def setup
-    season_id = 500
-    season_start_date = most_recent_september_1st
-    season_end_date = season_start_date + 1.year - 1.day
-    start_date = season_start_date + 1.day
-    season_query = <<~SQL
-      insert into seasons (id, start, "end") 
-      values (#{season_id}, '#{season_start_date.strftime("%Y-%m-%d")}', '#{season_end_date.strftime("%Y-%m-%d")}');
-    SQL
-    ActiveRecord::Base.connection.exec_query(season_query)
 
-    roster_query = <<~SQL
-      insert into base_rosters (player_id, team_id, start_date, season_id)
-      values (4270, 49804, '#{start_date.strftime("%Y-%m-%d")}', #{season_id}),
-             (27403, 49804, '#{start_date.strftime("%Y-%m-%d")}', #{season_id});
-    SQL
-    ActiveRecord::Base.connection.exec_query(roster_query)
   end
 
-  def team_url
-    "/b/team/49804"
+  def team_url(team_id)
+    "/b/team/#{team_id}"
   end
 
   test "team page has its name and link to rating.chgk.info" do
-    visit team_url
+    team = teams(:team_8)
+    visit team_url(team.id)
 
-    assert_text "Борский корабел (Москва)"
-    assert_equal "Страница на rating.chgk.info", find_link(href: "https://rating.chgk.info/teams/49804").text
+    assert_text team.name
+    assert_equal "Страница на rating.chgk.info", find_link(href: "https://rating.chgk.info/teams/#{team.id}").text
   end
 
   test "team page has its base roster" do
-    visit team_url
+    team = teams(:team_8)
+    visit team_url(team.id)
 
     season_title = if Time.zone.today.month >= 9
       "#{Time.zone.today.year}/#{(Time.zone.today.year + 1).to_s.last(2)}"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,6 @@ ENV["RAILS_ENV"] = "test"
 
 require "minitest"
 require_relative "../config/environment"
+require "rails/test_help"
 require "capybara/rails"
 Capybara.default_driver = :rack_test

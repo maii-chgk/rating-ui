8e94a8a8a057164510bba588d78a93081568de81
Jury Razumau <mail@razumau.net>
2024-10-31 23:00:25 +0100

    don’t show NaN TrueDL
    `true_dls_by_team.values.sum / true_dls_by_team.size.to_f` returns `NaN` for empty hashes.

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -21,9 +21,9 @@ class TournamentsController < ApplicationController
     all_players = current_model.tournament_players(tournament_id: id)
     results.each { |tr| tr.players = all_players[tr["team_id"]] }
 
-    @true_dls_by_team = TrueDLCalculator.tournament_dl_by_team(tournament_id: id, model: current_model)
-    @true_dl = @true_dls_by_team.values.sum / @true_dls_by_team.size.to_f
+    true_dls_by_team = TrueDLCalculator.tournament_dl_by_team(tournament_id: id, model: current_model)
+    @true_dl = true_dls_by_team.values.sum / true_dls_by_team.size.to_f unless true_dls_by_team.empty?
 
-    @tournament = TournamentPresenter.new(id:, details:, results:, truedls: @true_dls_by_team)
+    @tournament = TournamentPresenter.new(id:, details:, results:, truedls: true_dls_by_team)
   end
 end
